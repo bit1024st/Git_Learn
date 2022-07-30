939e669040942734a5df603ccc03f94eb339110b
June <1373485899@qq.com>
2022-07-30 21:43:42 +0800

    hot-fix second commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld {
         System.out.println("HelloWorld");
         System.out.println("我爱我家");
         System.out.println("hot-fix branch modify");
+        System.out.println("Conflicts:hot-fix");
+
     }
 }
