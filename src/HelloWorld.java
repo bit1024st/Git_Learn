ca87f806b81b5361feea424dfb3dfb198a8eda3f
June <1373485899@qq.com>
2022-07-30 20:40:35 +0800

    hot-fix first commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("HelloWorld");
         System.out.println("我爱我家");
+        System.out.println("hot-fix branch modify");
     }
 }
